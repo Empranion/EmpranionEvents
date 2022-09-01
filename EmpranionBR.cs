637827c49e469a27fccb09ce3c12911ea20bead4
naka <joaovtnakamura@gmail.com>
2022-08-31 21:52:09 -0300

    Base project files

---
+++ EmpranionBR.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace EmpranionBR;
+
+public sealed class EmpranionBR : Mod
+{
+    public static EmpranionBR Instance { get; } = ModContent.GetInstance<EmpranionBR>();
+}
---
+++ EmpranionBRPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Steamworks;
+using Terraria.ModLoader;
+
+namespace EmpranionBR;
+
+public sealed class EmpranionBRPlayer : ModPlayer
+{
+    public static readonly ulong[] AdminIds = {
+        76561198804876877,
+        76561198845331461
+    };
+
+    public static bool IsAdmin { get; } = AdminIds.Contains(SteamUser.GetSteamID().m_SteamID);
+}
