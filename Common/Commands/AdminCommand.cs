c53eadc2a122de830d28f5fb02d0000602f34a82
naka <joaovtnakamura@gmail.com>
2022-09-02 19:51:54 -0300

    Localization + Base for commands

---
+++ Common/Commands/AdminCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using EmpranionBR.Common.Id;
+using Steamworks;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace EmpranionBR.Common.Commands;
+
+public abstract class AdminCommand : ModCommand
+{
+    public override sealed CommandType Type { get; } = CommandType.Chat;
+
+    public override sealed void Action(CommandCaller caller, string input, string[] args) {
+        try {
+            if (!AdminId.AllIds.Contains(SteamUser.GetSteamID().m_SteamID)) {
+                caller.Reply(Language.GetTextValue("Mods.EmpranionBR.Commands.PermissionErrorMessage"), ColorId.CommandErrorColor);
+                return;
+            }
+
+            SafeAction(caller, input, args);
+        }
+        catch (UsageException exception) {
+            caller.Reply(Language.GetTextValue("Mods.EmpranionBR.Commands.ExecutionErrorMessage"), ColorId.CommandErrorColor);
+        }
+    }
+
+    public abstract void SafeAction(CommandCaller caller, string input, string[] args);
+}
---
+++ Common/Id/ColorId.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace EmpranionBR.Common.Id;
+
+public static class ColorId
+{
+    public static Color CommandErrorColor = new(243, 99, 99);
+    public static Color CommandSuccessColor = new(159, 246, 140);
+}
