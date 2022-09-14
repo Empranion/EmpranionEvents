dcc8b6d8fa8de858e481e350ce12042f95dc48ae
naka <joaovtnakamura@gmail.com>
2022-09-14 17:33:54 -0300

    Stat changes for projectiles and items

---
+++ Common/Globals/EmpranionGlobalProjectile.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EmpranionBR.Common.Globals;
+
+public sealed class EmpranionGlobalProjectile : GlobalProjectile
+{
+    public override void SetDefaults(Projectile projectile) {
+        switch (projectile.type) {
+            case ProjectileID.Starfury:
+                projectile.damage = 32;
+                projectile.velocity *= 14;
+                break;
+        }
+    }
+}
